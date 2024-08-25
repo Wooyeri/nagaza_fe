7fc1b2556567f7dea62faa981a8e67b1e00d378b
Hoyeon <86115801+Hoyeon9@users.noreply.github.com>
2024-08-26 02:40:46 +0900

    Add header

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,14 @@
+import "./Header.css"
+
+export default function Header(){
+    return(
+        <div className="header">
+            <div className="logo">
+                <img />
+            </div>
+            <div className="avatar">
+                    <img />
+            </div>
+        </div>
+    )
+}
