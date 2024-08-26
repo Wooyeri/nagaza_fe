5339262e8fbd5d079839656c3debc135904a3312
Hoyeon <86115801+Hoyeon9@users.noreply.github.com>
2024-08-26 15:00:38 +0900

    Add ListPage
    * Add search bar
    * Add category buttons

---
+++ src/components/ListPage.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react"
+import "./button.css"
+import searchIcon from "@/assets/search_icon.svg"
+import movie from "@/assets/movie.svg"
+import hotel from "@/assets/hotel.svg"
+import restaurant from "@/assets/restaurant.svg"
+
+
+export default function ListPage(){
+    const [search, setSearch] = useState('')
+    const [curCategory, setCurCategory] = useState('')
+
+    const containerStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    }
+    const buttonStyle = {
+        width: '60px',
+        height: '60px',
+        borderRadius: '50px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        cursor: 'pointer',
+        margin: '0 60px'
+    }
+    const buttonFont = {
+        textAlign: 'center',
+        fontWeight: 'extra bold',
+        fontSize: '16px',
+        margin: '15px'
+    }
+    return(
+        <div className="list-page-container" style={containerStyle}>
+            <div className="category-btns" style={{display: 'flex', flexDirection: 'row', alignItems: 'center', margin: '60px'}}>
+                <div className="movie">
+                    <div className={curCategory === 'movie' ? "selected-btn" : "category-btn"} onClick={() => {setCurCategory('movie')}} style={buttonStyle}><img src={movie} style={{width: "30px"}} /></div>
+                    <div style={buttonFont}>영화</div>
+                </div>
+                <div className="hotel">
+                    <div className={curCategory === 'hotel' ? "selected-btn" : "category-btn"} onClick={() => {setCurCategory('hotel')}} style={buttonStyle}><img src={hotel} style={{width: "30px"}} /></div>
+                    <div style={buttonFont}>숙박</div>
+                </div>
+                <div className="restaurant">
+                    <div className={curCategory === 'restaurant' ? "selected-btn" : "category-btn"} onClick={() => {setCurCategory('restaurant')}} style={buttonStyle}><img src={restaurant} style={{width: "30px"}} /></div>
+                    <div style={buttonFont}>식당</div>
+                </div>
+            </div>
+            <div className="search-area" style={{display: 'flex', flexDirection: 'row', alignItems: 'center', width: '70%', padding: '10px 10px 10px 15px', border: '1px solid #9E9E9E', borderRadius: '30px'}}>
+                <img src={searchIcon} />
+                <input type="text" value={search} placeholder="Search..." onChange={(e) => {setSearch(e.target.value)}} style={{width: '80%', fontSize: '20px', border: 'none', margin: '0 3%'}} />
+            </div>
+        </div>
+    )
+}
