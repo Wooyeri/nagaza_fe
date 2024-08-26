cd2af6d12e56f478d5552a35525459c1b4fe1cec
Hoyeon <86115801+Hoyeon9@users.noreply.github.com>
2024-08-26 13:01:05 +0900

    Add ListMovieCard
    * detach gauge bar from ListCard

--- src/components/card/ListCard.jsx
+++ src/components/card/ListCard.jsx
@@ -1,6 +1,6 @@
-import PropTypes from 'prop-types';
 import { useState, useEffect } from "react"
 import './card.css'
+import EmotionGauge from "./EmotionGauge"
 import likeIcon from '@/assets/like_icon.svg'
 import scrapIcon from '@/assets/scrap_icon.svg'
 import robotIcon from '@/assets/robot_icon.svg'
@@ -53,40 +53,4 @@ export default function ListCard(){
             </div>
         </div>
     )
-}
-
-function EmotionGauge({emotion_rating}){
-    const [position, setPosition] = useState(0);
-    const [emoji, setEmoji] = useState('');
-    useEffect(()=>{
-        setPosition((emotion_rating - 5.54) + '%');
-
-        if (emotion_rating >= 0 && emotion_rating < 20){
-            setEmoji('😡');
-        } else if (emotion_rating < 40){
-            setEmoji('😞');
-        } else if (emotion_rating < 60){
-            setEmoji('😐');
-        } else if (emotion_rating < 80){
-            setEmoji('😊');
-        } else if (emotion_rating <= 100){
-            setEmoji('😍');
-        }
-    }, [])
-
-    const gaugeBar = {
-        display: 'flex',
-        width: '250px',
-        height: '30px',
-        background: 'linear-gradient(90deg, rgba(255, 0, 0, 0.45) 0%, rgba(251, 141, 13, 0.45) 27%, rgba(250, 255, 21, 0.45) 50.91%, rgba(191, 255, 54, 0.45) 71.69%, rgba(8, 255, 3, 0.45) 92.47%)',
-        borderRadius: '8px',
-    };
-    return(
-        <div style={{...gaugeBar, flexDirection:'column', justifyContent: 'center'}}>
-            <div style={{marginLeft: position, fontSize: '20px', width: 'fit-content'}}>{emoji}</div>
-        </div>
-    )
-}
-EmotionGauge.propTypes = {
-    emotion_rating: PropTypes.number
 }
