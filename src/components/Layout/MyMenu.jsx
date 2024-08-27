211102cbf4ab115f334ff6db9917379ca3d2f975
Hoyeon <86115801+Hoyeon9@users.noreply.github.com>
2024-08-28 00:37:30 +0900

    Add icons and slider

--- src/components/Layout/MyMenu.jsx
+++ src/components/Layout/MyMenu.jsx
@@ -1,20 +1,55 @@
+import { useState } from 'react';
 import './MyMenu.css';
+import person from '@/assets/person.svg';
+import like from '@/assets/like_gray.svg';
+import bookmark from '@/assets/bookmark_gray.svg';
+import lightbulb from '@/assets/lightbulb.svg';
+import logout from '@/assets/logout.svg';
 
 export default function MyMenu() {
+  const [darkTheme, setDarkTheme] = useState(false);
 
   return (
     <div className='mymenu-list-container'>
       <h1>마이페이지</h1>
       <ul className="mymenu-list">
-        <li className='color'><div>개인 정보</div></li>
+        <li className='color'>
+          <div className='option'>
+            <img src={person} alt='person-icon' />
+            <div className='text'>개인 정보</div>
+          </div>
+        </li>
         <div className='divider'></div>
-        <li className='color'><div>좋아요</div></li>
+        <li className='color'>
+          <div className='option'>
+            <img src={like} alt='heart-icon' />
+            <div className='text'>좋아요</div>
+          </div>
+        </li>
         <div className='divider'></div>
-        <li className='color'><div>스크랩</div></li>
+        <li className='color'>
+          <div className='option'>
+            <img src={bookmark} alt='bookmark-icon' />
+            <div className='text'>스크랩</div>
+          </div>
+        </li>
         <div className='divider'></div>
-        <li><div>다크 테마</div></li>
+        <li style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
+          <div className='option'>
+            <img src={lightbulb} alt='lightbulb-icon' />
+            <div className='text'>다크 테마</div>
+          </div>
+          <div className='slider-wrapper'>
+            <div className={`slider ${darkTheme ? 'on' : 'off'}`} onClick={() => {setDarkTheme(!darkTheme)}}><div className='ball'></div></div>
+          </div>
+        </li>
         <div className='divider'></div>
-        <li className='color'><div>로그아웃</div></li>
+        <li className='color'>
+          <div className='option'>
+            <img src={logout} alt='logout-icon'/>
+            <div className='text'>로그아웃</div>
+          </div>
+        </li>
       </ul>
     </div>
   );
