0dc0ced4749f52fa8c72ca3d892b06b4efed020e
Hoyeon <86115801+Hoyeon9@users.noreply.github.com>
2024-08-27 19:06:11 +0900

    Add Login stuff

---
+++ src/components/signUp/SignUp.jsx
@@ -0,0 +1,99 @@
+import { useState } from 'react';
+import './SignUp.css';
+
+function SignUp() {
+  const [form, setForm] = useState({
+    firstName: '',
+    lastName: '',
+    id: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prevForm) => ({
+      ...prevForm,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //Todo: 백엔드 연결
+    console.log('Form submitted:', form);
+  };
+
+  return (
+    <div className="signup-container">
+      <form className="signup-form" onSubmit={handleSubmit}>
+        <h2>Sign Up</h2>
+        <div className="form-group">
+          <input
+            type="text"
+            name="firstName"
+            placeholder="First Name"
+            value={form.firstName}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Last Name"
+            value={form.lastName}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            name="id"
+            placeholder="ID"
+            value={form.id}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={form.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={form.password}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            name="confirmPassword"
+            placeholder="Confirm Password"
+            value={form.confirmPassword}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit" className="submit-button">Sign Up</button>
+        <div className="google-signup">
+          <button className="google-button">
+            <span className="google-icon">G</span> Sign up with Google
+          </button>
+        </div>
+        <div className="login-link">
+          Already have an account? <a href="/login">Sign in</a>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default SignUp;
