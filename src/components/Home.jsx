2dbe3c9c9d139d7692c100a691b445f89dc9ae50
Hoyeon <86115801+Hoyeon9@users.noreply.github.com>
2024-08-27 13:18:26 +0900

    Separate main category buttons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,4 @@
-import movie from "@/assets/movie.svg"
-import hotel from "@/assets/hotel.svg"
-import restaurant from "@/assets/restaurant.svg"
-import "./button.css"
-import { Link } from "react-router-dom"
+import CategoryLinkButtons from "./CategoryLinkButtons"
 
 export default function Home(){
     const containerStyle = {
@@ -10,22 +6,6 @@ export default function Home(){
         flexDirection: 'column',
         alignItems: 'center',
         margin: "150px 0"
-    }
-    const buttonStyle = {
-        width: '60px',
-        height: '60px',
-        borderRadius: '50px',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        cursor: 'pointer',
-        margin: '0 60px'
-    }
-    const buttonFont = {
-        textAlign: 'center',
-        fontWeight: 'extra bold',
-        fontSize: '16px',
-        margin: '15px'
     }
 
     return(
@@ -35,26 +15,7 @@ export default function Home(){
                 <h1 style={{fontWeight: '600', fontSize: '48px', lineHeight: '58px', color: '#212121'}}>나가자!</h1>
                 <div><span style={{fontWeight: '400', fontSize: '18px', lineHeight: '24px', color: '#757575'}}>분야별 리뷰 AI 요약시스템</span></div>
             </div>
-            <div className="category-btns" style={{display: 'flex', flexDirection: 'row', alignItems: 'center', margin: '60px'}}>
-                <div className="movie">
-                    <Link to={"/lists/movie"}>
-                        <div className="category-btn" style={buttonStyle}><img src={movie} style={{width: "30px"}} /></div>
-                    </Link>
-                    <div style={buttonFont}>영화</div>
-                </div>
-                <div className="hotel">
-                    <Link to={"/lists/hotel"}>
-                        <div className="category-btn" style={buttonStyle}><img src={hotel} style={{width: "30px"}} /></div>
-                    </Link>
-                    <div style={buttonFont}>숙박</div>
-                </div>
-                <div className="restaurant">
-                    <Link to={"/lists/restaurant"}>
-                        <div className="category-btn" style={buttonStyle}><img src={restaurant} style={{width: "30px"}} /></div>
-                    </Link>
-                    <div style={buttonFont}>식당</div>
-                </div>
-            </div>
+            <CategoryLinkButtons />
         </div>
     )
 }
