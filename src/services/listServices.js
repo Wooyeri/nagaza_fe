f3e58b80d74b17339d1c8531ed0298d6fa54430a
Hoyeon <86115801+Hoyeon9@users.noreply.github.com>
2024-08-29 22:38:46 +0900

    remove comment

--- src/services/listServices.js
+++ src/services/listServices.js
@@ -2,10 +2,7 @@ import axios from "axios";
 
 const LIST_BASE_URL = import.meta.env.VITE_API_URL + '/api';
 
-export const getMovieLists = (token) => {
-    console.log(token)
-    return axios.get(LIST_BASE_URL + '/movie', {headers: {Authorization: `Bearer ${token}`}});
-}
+export const getMovieLists = (token) => axios.get(LIST_BASE_URL + '/movie', {headers: {Authorization: `Bearer ${token}`}});
 export const getMovieDetails = (token, id) => axios.get(LIST_BASE_URL + `/movie/${id}`, {headers: {Authorization: `Bearer ${token}`}});
 
 export const getHotelLists = (token) => axios.get(LIST_BASE_URL + '/hotel', {headers: {Authorization: `Bearer ${token}`}});
