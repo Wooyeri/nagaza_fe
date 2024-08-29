d53e8290ba3c1c666463b979a0d67b14b860ecfd
Hoyeon <86115801+Hoyeon9@users.noreply.github.com>
2024-08-30 07:37:15 +0900

    Put default message for empty likes

--- src/components/myList/LikeList.jsx
+++ src/components/myList/LikeList.jsx
@@ -1,34 +1,32 @@
+import { useContext, useEffect, useState } from 'react';
 import MyListDetail from './listDetail/MyListDetail';
-import { useEffect, useState } from 'react';
+import { ThemeContext } from "@/common/Context"
 import { getLikeList } from '@/services/likeServices';
-import { testHotel } from '../testData';
 
+import { lightPallete, darkPallete } from "../../assets/pallete";
 import './scrapList.css';
 
 export default function LikeList() {
-  const [ likeList, setLikeList ] = useState([]);
+  const { darkMode } = useContext(ThemeContext);
+  const [ likeItems, setLikeItems ] = useState([]);
   const categories = ['movie', 'hotel', 'restaurant'];
-
-  const tempData =  [
-    testHotel[0], testHotel[1]
-  ]
 
   useEffect(() => {
     categories.map((category) => {
       getLikeList(category, sessionStorage.getItem('jwtToken'))
       .then(res => {
-        if (res.status == 200 && res.data && res.data.length > 0) {setLikeList([...likeList, res.data]);}
+        if (res.status == 200 && res.data && res.data.length > 0) {setLikeItems([...likeItems, res.data]);}
       })
       .catch(err => console.error(err));
     })
 
-  }, [likeList])
+  }, [likeItems])
 
   return (
     <div style={{width: "70%", display: "flex", flexDirection: "column", alignItems: "center"}}>
       <div style={{display: "flex", flexDirection: "column"}}>
         {/*Todo: key를 id로 교체해야함*/}
-        {tempData && tempData.map((contents, idx) => <MyListDetail key={idx} contents={contents} />)}
+        {likeItems ? <p style={{fontSize: "1.5rem" ,color: darkMode ? darkPallete.plainText : lightPallete.plainText}}>❤️를 눌러 좋아하는 항목을 저장하세요.</p> : likeItems.map((contents, idx) => <MyListDetail key={idx} contents={contents} />)}
         </div>
     </div>
   );
