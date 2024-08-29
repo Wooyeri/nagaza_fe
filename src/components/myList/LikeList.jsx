faec582419e444c6deeeacaf69bd042526c66041
Hoyeon <86115801+Hoyeon9@users.noreply.github.com>
2024-08-30 02:34:07 +0900

    Create skeleton for like apis

--- src/components/myList/LikeList.jsx
+++ src/components/myList/LikeList.jsx
@@ -1,12 +1,18 @@
 import './scrapList.css';
 import LikeDetail from './listDetail/LikeDetail';
 import { testHotel } from '../testData';
+import { useEffect } from 'react';
+import { getLikeList } from '@/services/likeServices';
 
 export default function LikeList() {
   //Todo: BE에서 가져오기
   const likeLists = [
     testHotel[0], testHotel[1]
   ]
+
+  useEffect(() => {
+    getLikeList('movie', sessionStorage.getItem('jwtToken')).then(res => console.log(res)).catch(err => console.error(err));
+  })
 
   return (
     <div style={{width: "70%", display: "flex", flexDirection: "column", alignItems: "center"}}>
--- src/services/likeServices.js
+++ src/services/likeServices.js
@@ -2,6 +2,14 @@ import axios from "axios";
 
 const LIKE_BASE_URL = import.meta.env.VITE_API_URL + '/api';
 
-export const getLikeLists = (token) => axios.get(LIKE_BASE_URL + '/like', {
-    headers: { Authorization: `Bearer ${token}` },
+export const getLikeList = (category, token) => axios.get(`${LIKE_BASE_URL}/${category}/likes`, {
+    headers: {Authorization: `Bearer ${token}`}
+});
+
+export const saveLike = (userId, category, itemId) => axios.post(`${LIKE_BASE_URL}/${category}/like/${itemId}?userId=${userId}`, {
+    //headers: { 'Content-Type': 'application/json' },
+});
+
+export const getIsLiked = (userId, category, itemId) => axios.get(`${LIKE_BASE_URL}/${category}/like/${itemId}?userId=${userId}`, {
+    //headers: { 'Content-Type': 'application/json' },
 });
