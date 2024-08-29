3649489fe65d0155936fd995e9006c7c931785e4
Hoyeon <86115801+Hoyeon9@users.noreply.github.com>
2024-08-30 03:38:59 +0900

    Set LikeList with api

--- src/components/myList/LikeList.jsx
+++ src/components/myList/LikeList.jsx
@@ -1,24 +1,28 @@
 import './scrapList.css';
 import LikeDetail from './listDetail/LikeDetail';
-import { testHotel } from '../testData';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getLikeList } from '@/services/likeServices';
 
 export default function LikeList() {
-  //Todo: BE에서 가져오기
-  const likeLists = [
-    testHotel[0], testHotel[1]
-  ]
+  const [ likeList, setLikeList ] = useState([]);
+  const categories = ['movie', 'hotel', 'restaurant'];
 
   useEffect(() => {
-    getLikeList('movie', sessionStorage.getItem('jwtToken')).then(res => console.log(res)).catch(err => console.error(err));
-  })
+    categories.map((category) => {
+      getLikeList(category, sessionStorage.getItem('jwtToken'))
+      .then(res => {
+        if (res.data && res.data.length > 0) setLikeList([...likeList, res.data]);
+      })
+      .catch(err => console.error(err));
+    })
+
+  }, [likeList])
 
   return (
     <div style={{width: "70%", display: "flex", flexDirection: "column", alignItems: "center"}}>
       <div style={{display: "flex", flexDirection: "column"}}>
         {/*Todo: key를 id로 교체해야함*/}
-        {likeLists.map((contents, idx) => <LikeDetail key={idx} contents={contents} />)}
+        {likeList && likeList.map((contents, idx) => <LikeDetail key={idx} contents={contents} />)}
         </div>
     </div>
   );
--- src/services/likeServices.js
+++ src/services/likeServices.js
@@ -3,13 +3,13 @@ import axios from "axios";
 const LIKE_BASE_URL = import.meta.env.VITE_API_URL + '/api';
 
 export const getLikeList = (category, token) => axios.get(`${LIKE_BASE_URL}/${category}/likes`, {
-    headers: {Authorization: `Bearer ${token}`}
+    headers: {Authorization: `Bearer ${token}`},
 });
 
-export const saveLike = (userId, category, itemId) => axios.post(`${LIKE_BASE_URL}/${category}/like/${itemId}?userId=${userId}`, {
-    //headers: { 'Content-Type': 'application/json' },
+export const saveLike = (category, itemId, token) => axios.post(`${LIKE_BASE_URL}/${category}/like/${itemId}`, {
+    headers: {Authorization: `Bearer ${token}`},
 });
 
-export const getIsLiked = (userId, category, itemId) => axios.get(`${LIKE_BASE_URL}/${category}/like/${itemId}?userId=${userId}`, {
-    //headers: { 'Content-Type': 'application/json' },
+export const getIsLiked = (category, itemId, token) => axios.get(`${LIKE_BASE_URL}/${category}/like/${itemId}`, {
+    headers: {Authorization: `Bearer ${token}`},
 });
